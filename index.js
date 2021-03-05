17e0c5b5e22b4e5af41951d3b689035d316f1404
Ruben Mesta <rubenmesta29@gmail.com>
2021-03-05 17:14:35 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+require('./routes/dialogFlowRoutes')(app);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is up at port: ${PORT}`)
+})
